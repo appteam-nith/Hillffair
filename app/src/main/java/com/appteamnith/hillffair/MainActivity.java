c919f28c83541a207cfc45b0a97773e4f23f7b27
Aditya Arora <aadityaaaaroraa@gmail.com>
2016-08-31 09:01:03 +0530

    Login Page(Portrait & Landscape) added

--- app/src/main/java/com/appteamnith/hillffair/MainActivity.java
+++ app/src/main/java/com/appteamnith/hillffair/MainActivity.java
@@ -2,12 +2,51 @@ package com.appteamnith.hillffair;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo;
+    ScrollView layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        logo = (ImageView) findViewById(R.id.logo);
+        layout = (ScrollView) findViewById(R.id.layout);
+        layout.setVisibility(View.INVISIBLE);
+        final Animation bounceLogo = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.loginlogo_anim);
+        final Animation layoutFade = AnimationUtils.loadAnimation(getApplicationContext(),android.R.anim.fade_in);
+
+
+        logo.startAnimation(bounceLogo);
+        bounceLogo.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                layout.startAnimation(layoutFade);
+                layout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
     }
 }
