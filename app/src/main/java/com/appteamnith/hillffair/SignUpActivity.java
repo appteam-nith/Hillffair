87353625b0323578d3dfb785078e3ec40c66792a
RamolaWeb <sramola5@gmail.com>
2016-09-10 11:34:44 +0530

    Link Login Activity with Sign up Activity and make an Main Activity.

--- app/src/main/java/com/appteamnith/hillffair/SignUpActivity.java
+++ app/src/main/java/com/appteamnith/hillffair/SignUpActivity.java
@@ -2,11 +2,15 @@ package com.appteamnith.hillffair;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
