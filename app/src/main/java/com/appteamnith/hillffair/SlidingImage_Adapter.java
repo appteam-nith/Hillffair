a65ce91ec852f0f5b53e1d0e27263edd64f8e194
octacode <allblue.shasha@gmail.com>
2016-10-01 22:55:48 +0530

    Addition of Image Slider in Home Activity

---
+++ app/src/main/java/com/appteamnith/hillffair/SlidingImage_Adapter.java
@@ -0,0 +1,74 @@
+package com.appteamnith.hillffair;
+
+/**
+ * Created by Kumar Shashwat on 01-Oct-16.
+ */
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Octa_Code on 28-Sep-16.
+ */
+public class SlidingImage_Adapter extends PagerAdapter {
+
+
+    private ArrayList<Integer> IMAGES;
+    private LayoutInflater inflater;
+    private Context context;
+
+
+    public SlidingImage_Adapter(Context context,ArrayList<Integer> IMAGES) {
+        this.context = context;
+        this.IMAGES=IMAGES;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return IMAGES.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = inflater.inflate(R.layout.fragment_back_drop, view, false);
+
+        assert imageLayout != null;
+        final ImageView imageView = (ImageView) imageLayout.findViewById(R.id.image_backdrop);
+
+
+        imageView.setImageResource(IMAGES.get(position));
+
+        view.addView(imageLayout, 0);
+
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+
+
+}
