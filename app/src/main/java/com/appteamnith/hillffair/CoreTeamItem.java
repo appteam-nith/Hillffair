cc3920118ff089ed69e6a2496c1b81532948aeb5
iamNitin16 <nitinsharma95ns@gmail.com>
2016-10-03 01:39:19 +0530

    Core Team Activity is Modified

--- app/src/main/java/com/appteamnith/hillffair/CoreTeamItem.java
+++ app/src/main/java/com/appteamnith/hillffair/CoreTeamItem.java
@@ -1,7 +1,13 @@
 package com.appteamnith.hillffair;
 
 /**
- * Created by root on 10/3/16.
+ * Created by Nitin on 9/28/16.
  */
 public class CoreTeamItem {
+    public String name,designation,url;
+    CoreTeamItem(String name,String designation,String url){
+        this.name=name;
+        this.designation=designation;
+        this.url=url;
+    }
 }
