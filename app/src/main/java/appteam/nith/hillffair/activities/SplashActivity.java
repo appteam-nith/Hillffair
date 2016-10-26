1bf7774625564da32418ab3fd08637a8bec1796c
RamolaWeb <sramola5@gmail.com>
2016-10-26 23:43:21 +0530

    changed splash and some layout modifications

--- app/src/main/java/appteam/nith/hillffair/activities/SplashActivity.java
+++ app/src/main/java/appteam/nith/hillffair/activities/SplashActivity.java
@@ -4,16 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
-import appteam.nith.hillffair.Notification.DbHelper;
-import appteam.nith.hillffair.R;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.target.Target;
+
+import appteam.nith.hillffair.R;
 
 public class SplashActivity extends AppCompatActivity {
     private static final long TIME_SPLASH =1500 ;
@@ -25,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         image_splash = (ImageView) findViewById(R.id.image_spalsh);
-        Target<GlideDrawable> into = Glide.with(SplashActivity.this).load(R.drawable.splash_new).into(image_splash);
+        Target<GlideDrawable> into = Glide.with(SplashActivity.this).load(R.drawable.splash_new2).into(image_splash);
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
